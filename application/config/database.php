8c988c1bcd16b04d98ef017d684d33147c9bc23e
ChandlerVictorino <166820472+ChandlerVictorino@users.noreply.github.com>
2025-06-07 05:37:55 +0800

    Update database.php

--- application/config/database.php
+++ application/config/database.php
@@ -76,7 +76,6 @@ $query_builder = TRUE;
 $db['default'] = array(
 	'dsn'	=> '',
 	'hostname' => 'formula1-carrental-ntc-7bab.f.aivencloud.com',
-	'port' => '13275',
 	'username' => 'avnadmin',
 	'password' => 'AVNS_0w8vvyWyzFsoZ0Pt6qx',
 	'database' => 'defaultdb',
