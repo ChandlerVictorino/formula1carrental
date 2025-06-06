2de36a2e584fdbe505540af471069dcb130e0d5d
ChandlerVictorino <166820472+ChandlerVictorino@users.noreply.github.com>
2025-06-07 06:37:33 +0800

    Update database.php

--- application/config/database.php
+++ application/config/database.php
@@ -75,11 +75,10 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'formula1-carrental-ntc-7bab.f.aivencloud.com', 
-	'port' => '13271',
-	'username' => 'avnadmin',
-	'password' => 'AVNS_0w8vvyWyzFsoZ0Pt6qx',
-	'database' => 'defaultdb',
+	'hostname' => 'localhost', 
+	'username' => 'root',
+	'password' => '',
+	'database' => 'rental_mobile',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
