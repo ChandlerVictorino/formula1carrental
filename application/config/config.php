06d525d897e1d7dee724d3ac2624db4210e3a207
ChandlerVictorino <166820472+ChandlerVictorino@users.noreply.github.com>
2025-06-07 09:06:19 +0800

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/Formula1_CarRental/';
+$config['base_url'] = 'sql12.freesqldatabase.com';
 
 /*
 |--------------------------------------------------------------------------
