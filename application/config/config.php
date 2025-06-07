7a3597dc73b34d80d4bbf979f59137683e12b594
ChandlerVictorino <166820472+ChandlerVictorino@users.noreply.github.com>
2025-06-07 09:09:35 +0800

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'sql12.freesqldatabase.com';
+$config['base_url'] = 'https://formula1carrental.onrender.com';
 
 /*
 |--------------------------------------------------------------------------
