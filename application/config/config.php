7a9a5c0adc1e905b9e753403f353c8841aced10d
ChandlerVictorino <166820472+ChandlerVictorino@users.noreply.github.com>
2025-06-08 08:03:34 +0800

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://formula1carrental.onrender.com';
+$config['base_url'] = 'https://formula1carrental.onrender.com/';
 
 /*
 |--------------------------------------------------------------------------
@@ -401,10 +401,10 @@ $config['sess_regenerate_destroy'] = FALSE;
 |
 */
 $config['cookie_prefix']	= '';
-$config['cookie_domain']	= '';
+$config['cookie_domain']	= 'formula1carrental.onrender.com';
 $config['cookie_path']		= '/';
-$config['cookie_secure']	= FALSE;
-$config['cookie_httponly'] 	= FALSE;
+$config['cookie_secure']	= TRUE;
+$config['cookie_httponly'] 	= TRUE;
 
 /*
 |--------------------------------------------------------------------------
