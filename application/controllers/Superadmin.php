90062754393bf2c9bcb7e7ffa06d81b9e9449c30
ChandlerVictorino <166820472+ChandlerVictorino@users.noreply.github.com>
2025-06-13 05:28:55 +0800

    Update Superadmin.php

--- application/controllers/Superadmin.php
+++ application/controllers/Superadmin.php
@@ -7,12 +7,12 @@ class Superadmin extends CI_Controller {
         parent::__construct();
         $this->load->model('m_rental');
         if ($this->session->userdata('role') !== 'superadmin') {
-            redirect('welcome?pesan=forbidden');
+            redirect('welcome');
         }
     }
 
     public function dashboard() {
-        $data['admins'] = $this->m_rental->get_data('admin')->result();
+        $data['admin'] = $this->m_rental->get_data('admin')->result();
         $this->load->view('superadmin/dashboard', $data);
     }
 
@@ -21,37 +21,40 @@ class Superadmin extends CI_Controller {
     }
 
     public function store_admin() {
-        $data = [
+        $data = array(
             'admin_name' => $this->input->post('admin_name'),
             'admin_username' => $this->input->post('admin_username'),
             'admin_password' => md5($this->input->post('admin_password'))
-        ];
+        );
+
         $this->m_rental->insert_data($data, 'admin');
         redirect('superadmin/dashboard');
     }
 
     public function edit_admin($id) {
-        $where = ['admin_id' => $id];
+        $where = array('admin_id' => $id);
         $data['admin'] = $this->m_rental->edit_data($where, 'admin')->row();
         $this->load->view('superadmin/edit_admin', $data);
     }
 
     public function update_admin() {
-        $id = $this->input->post('admin_id');
-        $data = [
+        $admin_id = $this->input->post('admin_id');
+        $update_data = array(
             'admin_name' => $this->input->post('admin_name'),
             'admin_username' => $this->input->post('admin_username')
-        ];
-        if ($this->input->post('admin_password')) {
-            $data['admin_password'] = md5($this->input->post('admin_password'));
+        );
+
+        if (!empty($this->input->post('admin_password'))) {
+            $update_data['admin_password'] = md5($this->input->post('admin_password'));
         }
-        $where = ['admin_id' => $id];
-        $this->m_rental->update_data($where, $data, 'admin');
+
+        $where = array('admin_id' => $admin_id);
+        $this->m_rental->update_data($where, $update_data, 'admin');
         redirect('superadmin/dashboard');
     }
 
     public function delete_admin($id) {
-        $where = ['admin_id' => $id];
+        $where = array('admin_id' => $id);
         $this->m_rental->delete_data($where, 'admin');
         redirect('superadmin/dashboard');
     }
