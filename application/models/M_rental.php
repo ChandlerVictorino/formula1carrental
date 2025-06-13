5600bc34af107905ed9700a50eefef00dbfa6c88
ChandlerVictorino <166820472+ChandlerVictorino@users.noreply.github.com>
2025-06-14 05:33:53 +0800

    Update M_rental.php

--- application/models/M_rental.php
+++ application/models/M_rental.php
@@ -1,46 +1,36 @@
 <?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
 class M_rental extends CI_Model {
 
-    public function get_data($table) {
-        return $this->db->get($table);
-    }
-
-    public function insert_data($data, $table) {
-        $this->db->insert($table, $data);
-    }
-
-    public function update_data($where, $data, $table) {
-        $this->db->where($where);
-        $this->db->update($table, $data);
-    }
-
-    public function delete_data($where, $table) {
-        $this->db->where($where);
-        $this->db->delete($table);
-    }
-
-    public function edit_data($where, $table) {
-        return $this->db->get_where($table, $where);
-    }
-
-    public function update_superadmin_password($id, $new_password) {
-        $hashed_password = password_hash($new_password, PASSWORD_DEFAULT);
-        $this->db->where('superadmin_id', $id);
-        $this->db->update('superadmin', ['superadmin_password' => $hashed_password]);
+    public function get_admin_by_username($username) {
+        return $this->db->get_where('admin', ['admin_username' => $username])->row();
     }
 
     public function get_superadmin_by_username($username) {
         return $this->db->get_where('superadmin', ['superadmin_username' => $username])->row();
     }
 
-    public function get_admin_by_username($username) {
-        return $this->db->get_where('admin', ['admin_username' => $username])->row();
+    public function update_superadmin_password($id, $new_password) {
+        $hash = password_hash($new_password, PASSWORD_DEFAULT);
+        $this->db->where('superadmin_id', $id)
+                 ->update('superadmin', ['superadmin_password' => $hash]);
     }
 
-    public function check_superadmin($username, $password) {
-        return $this->db->get_where('superadmin', [
-            'superadmin_username' => $username,
-            'superadmin_password' => $password
-        ]);
+    // Basic CRUD for admin users
+    public function get_all_admins() {
+        return $this->db->get('admin')->result();
+    }
+    public function insert_admin($data) {
+        $this->db->insert('admin', $data);
+    }
+    public function get_admin($id) {
+        return $this->db->get_where('admin', ['admin_id' => $id])->row();
+    }
+    public function update_admin($id, $data) {
+        $this->db->where('admin_id', $id)->update('admin', $data);
+    }
+    public function delete_admin($id) {
+        $this->db->where('admin_id', $id)->delete('admin');
     }
 }
