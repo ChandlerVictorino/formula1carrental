240519b600e7195673e26321279b05211b22ec47
ChandlerVictorino <166820472+ChandlerVictorino@users.noreply.github.com>
2025-06-13 17:51:33 +0800

    Update Superadmin.php

--- application/controllers/Superadmin.php
+++ application/controllers/Superadmin.php
@@ -7,7 +7,6 @@ class Superadmin extends CI_Controller {
         parent::__construct();
         $this->load->model('m_rental');
 
-        // Redirect non-superadmins
         if ($this->session->userdata('role') !== 'superadmin') {
             redirect('welcome');
         }
@@ -23,18 +22,18 @@ class Superadmin extends CI_Controller {
     }
 
     public function store_admin() {
-        $data = array(
+        $data = [
             'admin_name' => $this->input->post('admin_name'),
             'admin_username' => $this->input->post('admin_username'),
             'admin_password' => md5($this->input->post('admin_password'))
-        );
+        ];
 
         $this->m_rental->insert_data($data, 'admin');
         redirect('superadmin/dashboard');
     }
 
     public function edit_admin($id) {
-        $where = array('admin_id' => $id);
+        $where = ['admin_id' => $id];
         $data['admin'] = $this->m_rental->edit_data($where, 'admin')->row();
         $this->load->view('superadmin/edit_admin', $data);
     }
@@ -42,16 +41,16 @@ class Superadmin extends CI_Controller {
     public function update_admin() {
         $admin_id = $this->input->post('admin_id');
 
-        $update_data = array(
+        $update_data = [
             'admin_name' => $this->input->post('admin_name'),
             'admin_username' => $this->input->post('admin_username')
-        );
+        ];
 
         if (!empty($this->input->post('admin_password'))) {
             $update_data['admin_password'] = md5($this->input->post('admin_password'));
         }
 
-        $where = array('admin_id' => $admin_id);
+        $where = ['admin_id' => $admin_id];
         $this->m_rental->update_data($where, $update_data, 'admin');
         redirect('superadmin/dashboard');
     }
@@ -59,10 +58,9 @@ class Superadmin extends CI_Controller {
     public function delete_confirmed() {
         $admin_id = $this->input->post('admin_id');
         $entered_password = md5($this->input->post('superadmin_password'));
-
         $username = $this->session->userdata('username');
 
-        $superadmin = $this->m_rental->check_login($username, $entered_password);
+        $superadmin = $this->m_rental->check_superadmin($username, $entered_password);
 
         if ($superadmin->num_rows() > 0) {
             $this->m_rental->delete_data(['admin_id' => $admin_id], 'admin');
@@ -74,4 +72,29 @@ class Superadmin extends CI_Controller {
         redirect('superadmin/dashboard');
     }
 
+    public function change_password() {
+        $this->load->view('superadmin/change_password');
+    }
+
+    public function update_password() {
+        $superadmin_id = $this->session->userdata('superadmin_id');
+        $old_password = md5($this->input->post('old_password'));
+        $new_password = $this->input->post('new_password');
+        $confirm_password = $this->input->post('confirm_password');
+        $username = $this->session->userdata('username');
+
+        $check = $this->m_rental->check_superadmin($username, $old_password);
+
+        if ($check->num_rows() == 0) {
+            $this->session->set_flashdata('error', 'Old password is incorrect.');
+            redirect('superadmin/change_password');
+        } elseif ($new_password != $confirm_password) {
+            $this->session->set_flashdata('error', 'New passwords do not match.');
+            redirect('superadmin/change_password');
+        } else {
+            $this->m_rental->update_superadmin_password($superadmin_id, $new_password);
+            $this->session->set_flashdata('success', 'Password updated successfully.');
+            redirect('superadmin/change_password');
+        }
+    }
 }
