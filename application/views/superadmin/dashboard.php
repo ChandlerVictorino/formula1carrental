42425da6347b8dd107fc26639e91a7a4c9362df4
ChandlerVictorino <166820472+ChandlerVictorino@users.noreply.github.com>
2025-06-14 03:11:31 +0800

    Update dashboard.php

--- application/views/superadmin/dashboard.php
+++ application/views/superadmin/dashboard.php
@@ -1,7 +1,7 @@
 <?php if ($this->session->flashdata('success')): ?>
     <div class="alert alert-success"><?php echo \$this->session->flashdata('success'); ?></div>
 <?php endif; ?>
-<?php if (\$this->session->flashdata('error')): ?>
+<?php if ($this->session->flashdata('error')): ?>
     <div class="alert alert-danger"><?php echo \$this->session->flashdata('error'); ?></div>
 <?php endif; ?>
 <!DOCTYPE html>
