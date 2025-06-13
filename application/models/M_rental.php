3c9d70eb5efa334dbc04ad61907e57888a1461ba
ChandlerVictorino <166820472+ChandlerVictorino@users.noreply.github.com>
2025-06-13 17:50:40 +0800

    Update M_rental.php

--- application/models/M_rental.php
+++ application/models/M_rental.php
@@ -1,35 +1,38 @@
 <?php
 class M_rental extends CI_Model {
-    
-    function edit_data($where, $table){
-        return $this->db->get_where($table, $where);
-    }
 
-    function get_data($table){
+    public function get_data($table){
         return $this->db->get($table);
     }
 
-    function insert_data($data, $table){
+    public function insert_data($data, $table){
         $this->db->insert($table, $data);
     }
 
-    function update_data($where, $data, $table){
+    public function edit_data($where, $table){
+        return $this->db->get_where($table, $where);
+    }
+
+    public function update_data($where, $data, $table){
         $this->db->where($where);
         $this->db->update($table, $data);
     }
 
-    function delete_data($where, $table){
+    public function delete_data($where, $table){
         $this->db->where($where);
         $this->db->delete($table);
     }
 
-    
-    public function check_login($username, $password) {
+    public function check_superadmin($username, $password) {
         return $this->db
-            ->where('admin_username', $username)
-            ->where('admin_password', $password)
-            ->where('role', 'superadmin') // optional if you want to strictly verify role
-            ->get('admin');
+            ->where('superadmin_username', $username)
+            ->where('superadmin_password', $password)
+            ->get('superadmin');
     }
-    
+
+    public function update_superadmin_password($superadmin_id, $new_password) {
+        return $this->db
+            ->where('superadmin_id', $superadmin_id)
+            ->update('superadmin', ['superadmin_password' => md5($new_password)]);
+    }
 }
