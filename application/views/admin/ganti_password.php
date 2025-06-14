3721c9099a8959f783de116cb7cde14f92728ebc
ChandlerVictorino <166820472+ChandlerVictorino@users.noreply.github.com>
2025-06-14 14:24:52 +0800

    Update ganti_password.php

--- application/views/admin/ganti_password.php
+++ application/views/admin/ganti_password.php
@@ -5,28 +5,31 @@
 
 <!-- Content Row -->
 <div class="row">
-    
     <div class="col-md-6 col-md-offset-3">
         <?php
-        if(isset($_GET['pesan'])){
-            if($_GET['pesan'] == 'berhasil'){
-                echo '<div class="alert alert-success">Password changed successfully!</div>';
-            }
+        if(isset($_GET['pesan']) && $_GET['pesan'] == 'berhasil'){
+            echo '<div class="alert alert-success">Password changed successfully!</div>';
         }
         ?>
+        
         <form action="<?php echo base_url().'admin/ganti_password_act'; ?>" method="post">
+            <!-- CSRF Token -->
+            <input type="hidden" name="<?= $this->security->get_csrf_token_name(); ?>"
+                   value="<?= $this->security->get_csrf_hash(); ?>">
+
             <div class="form-group">
-                <label>New password</label>
+                <label>New Password</label>
                 <input class="form-control" type="password" name="pass_baru">
                 <?php echo form_error('pass_baru'); ?>
             </div>
+
             <div class="form-group">
                 <label>Repeat New Password</label>
                 <input class="form-control" type="password" name="ulang_pass">
                 <?php echo form_error('ulang_pass'); ?>
             </div>
+
             <button type="submit" class="btn btn-primary">Submit</button>
         </form>
     </div>
-   
 </div>
