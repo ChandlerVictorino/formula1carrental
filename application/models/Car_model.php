8c60dc591d1bf4e6429025e6fd4752674368a44a
ChandlerVictorino <166820472+ChandlerVictorino@users.noreply.github.com>
2025-06-14 18:20:33 +0800

    Update Car_model.php

--- application/models/Car_model.php
+++ application/models/Car_model.php
@@ -7,24 +7,19 @@ class Car_model extends CI_Model {
         parent::__construct();
     }
 
-    // Fetch all cars
     public function get_all_cars() {
         return $this->db->get('mobile')->result();
     }
 
-    // Insert new car
     public function insert_car($data) {
         return $this->db->insert('mobile', $data);
     }
 
-// Get single car by ID
-public function get_car_by_id($id) {
-    return $this->db->get_where('mobile', ['mobile_id' => $id])->row();
-}
+    public function get_car_by_id($id) {
+        return $this->db->get_where('mobile', ['mobile_id' => $id])->row();
+    }
 
-// Delete car by ID
-public function delete_car($id) {
-    return $this->db->delete('mobile', ['mobile_id' => $id]);
-}
-
+    public function delete_car($id) {
+        return $this->db->delete('mobile', ['mobile_id' => $id]);
+    }
 }
