3175bffda0bfab8d4fb9ade337216f3a1df5fb24
ChandlerVictorino <166820472+ChandlerVictorino@users.noreply.github.com>
2025-06-14 19:02:16 +0800

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -449,7 +449,6 @@ $config['global_xss_filtering'] = FALSE;
 | 'csrf_exclude_uris' = Array of URIs which ignore CSRF checks
 */
 $config['csrf_protection'] = TRUE;
-$config['csrf_exclude_uris'] = array('api/cars/create'); // exclude your API POST route
 $config['csrf_token_name'] = 'csrf_test_name';
 $config['csrf_cookie_name'] = 'csrf_cookie_name';
 $config['csrf_expire'] = 7200;
