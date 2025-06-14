5b94e76e4c3defd7256f2fcfda440e8b55a0cc43
ChandlerVictorino <166820472+ChandlerVictorino@users.noreply.github.com>
2025-06-14 14:26:14 +0800

    Update mobile_add.php

--- application/views/admin/mobile_add.php
+++ application/views/admin/mobile_add.php
@@ -6,32 +6,42 @@
 <div class="card shadow mb-4">
     <div class="card-body">
         <form action="<?php echo base_url().'admin/mobile_add_act' ?>" method="post">
+            <!-- CSRF Protection -->
+            <input type="hidden" name="<?php echo $this->security->get_csrf_token_name(); ?>" 
+                   value="<?php echo $this->security->get_csrf_hash(); ?>" />
+                   
             <div class="form-group row">
                 <label class="col-sm-2 col-form-label">Name</label>
-                <div class="col-sm-10"><input type="text" class="form-control" name="carname" placeholder="ex. Jeep - Wrangler"></div>
+                <div class="col-sm-10">
+                    <input type="text" class="form-control" name="carname" placeholder="ex. Jeep - Wrangler">
+                </div>
                 <?php echo form_error('carname'); ?>
             </div>
             <div class="form-group row">
                 <label class="col-sm-2 col-form-label">Car Number</label>
-                <div class="col-sm-10"><input type="number, text" class="form-control" name="plate" placeholder="ex. DBA4658"></div>
+                <div class="col-sm-10">
+                    <input type="text" class="form-control" name="plate" placeholder="ex. DBA4658">
+                </div>
             </div>
             <div class="form-group row">
                 <label class="col-sm-2 col-form-label">Color</label>
-                <div class="col-sm-10"><input type="text" class="form-control" name="color" placeholder="ex. Matte Green"></div>
+                <div class="col-sm-10">
+                    <input type="text" class="form-control" name="color" placeholder="ex. Matte Green">
+                </div>
             </div>
             <div class="form-group row">
                 <label class="col-sm-2 col-form-label">Vehicle Year</label>
                 <div class="col-sm-10">
-                <input type="number" class="form-control" name="year" placeholder="ex. 2020">
+                    <input type="number" class="form-control" name="year" placeholder="ex. 2020">
                 </div>
             </div>
             <div class="form-group row">
                 <label class="col-sm-2 col-form-label">Car Status</label>
                 <div class="col-sm-10">
-                <select class="form-control" name="status">
-                    <option value="1">Available</option>
-                    <option value="0">Not Available</option>
-                </select>
+                    <select class="form-control" name="status">
+                        <option value="1">Available</option>
+                        <option value="0">Not Available</option>
+                    </select>
                 </div>
                 <?php echo form_error('status'); ?>
             </div>
