d0dc9a4468cc28a3af719570601cf9b396f0aa5e
ChandlerVictorino <166820472+ChandlerVictorino@users.noreply.github.com>
2025-06-14 14:27:30 +0800

    Update mobile_edit.php

--- application/views/admin/mobile_edit.php
+++ application/views/admin/mobile_edit.php
@@ -7,31 +7,44 @@
     <div class="card-body">
         <?php foreach($mobile as $m){ ?>
         <form action="<?php echo base_url().'admin/mobile_update' ?>" method="post">
+            <!-- CSRF Protection -->
+            <input type="hidden" name="<?php echo $this->security->get_csrf_token_name(); ?>" 
+                   value="<?php echo $this->security->get_csrf_hash(); ?>" />
+                   
             <input type="hidden" name="id" value="<?php echo $m->mobile_id; ?>">
+
             <div class="form-group row">
                 <label class="col-sm-2 col-form-label">Select Car</label>
-                <div class="col-sm-10"><input type="text" class="form-control" name="carname" value="<?php echo $m->mobile_carname; ?>"></div>
+                <div class="col-sm-10">
+                    <input type="text" class="form-control" name="carname" value="<?php echo $m->mobile_carname; ?>">
+                </div>
                 <?php echo form_error('carname'); ?>
             </div>
             <div class="form-group row">
                 <label class="col-sm-2 col-form-label">Plate Number</label>
-                <div class="col-sm-10"><input type="text" class="form-control" name="plate" value="<?php echo $m->mobile_plate; ?>"></div>
+                <div class="col-sm-10">
+                    <input type="text" class="form-control" name="plate" value="<?php echo $m->mobile_plate; ?>">
+                </div>
             </div>
             <div class="form-group row">
                 <label class="col-sm-2 col-form-label">Car Color</label>
-                <div class="col-sm-10"><input type="text" class="form-control" name="color" value="<?php echo $m->mobile_color; ?>"></div>
+                <div class="col-sm-10">
+                    <input type="text" class="form-control" name="color" value="<?php echo $m->mobile_color; ?>">
+                </div>
             </div>
             <div class="form-group row">
                 <label class="col-sm-2 col-form-label">Vehicle Year</label>
-                <div class="col-sm-10"><input type="text" class="form-control" name="year" value="<?php echo $m->mobile_year; ?>"></div>
+                <div class="col-sm-10">
+                    <input type="text" class="form-control" name="year" value="<?php echo $m->mobile_year; ?>">
+                </div>
             </div>
             <div class="form-group row">
                 <label class="col-sm-2 col-form-label">Status</label>
                 <div class="col-sm-10">
-                <select class="form-control" name="status">
-                    <option value="1"<?php echo ($m->mobile_status==1)?' selected="selected"':'' ?>>Available</option>
-                    <option value="0"<?php echo ($m->mobile_status==1)?'':' selected="selected"' ?>>Not Available</option>
-                </select>
+                    <select class="form-control" name="status">
+                        <option value="1"<?php echo ($m->mobile_status==1)?' selected="selected"':'' ?>>Available</option>
+                        <option value="0"<?php echo ($m->mobile_status==1)?'':' selected="selected"' ?>>Not Available</option>
+                    </select>
                 </div>
                 <?php echo form_error('status'); ?>
             </div>
